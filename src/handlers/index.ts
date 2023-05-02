10fc6581a97de52bfa432059343ccc18f6d4859e
Brandon Ellis <brandonellis.email@gmail.com>
2023-05-02 17:57:33 -0600

    organizing handlers in index

---
+++ src/handlers/index.ts
@@ -0,0 +1,9 @@
+import { handler as authHandler } from './auth';
+import { handler as connectionHandler } from './connection';
+import { handler as messageHandler } from './message';
+
+export {
+  authHandler,
+  connectionHandler,
+  messageHandler,
+};
