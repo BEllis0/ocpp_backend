1f97c70d927ce11208b120b53c16f3a0ad908bf8
Brandon Ellis <brandonellis.email@gmail.com>
2023-05-02 17:57:55 -0600

    moved handlers

---
+++ src/handlers/auth.ts
@@ -0,0 +1,4 @@
+export const handler = async (event, context) => ({
+  statusCode: 200,
+  body: 'connect',
+});
---
+++ src/handlers/message.ts
@@ -0,0 +1,50 @@
+import { aws } from '../lib';
+
+const sendResponse = async (client, connectionId, action, payload) => {
+  const body = {
+    action,
+    payload,
+  };
+
+  await client.postToConnection({
+    ConnectionId: connectionId,
+    Data: JSON.stringify(body),
+  }).promise();
+};
+
+export const handler = async (event, context) => {
+  const client = aws.apiGatewayClient(`${event.requestContext.domainName}/${event.requestContext.stage}`);
+  const { connectionId } = event.requestContext;
+  try {
+    const { action, payload } = JSON.parse(event.body);
+    console.log('action received: ', action);
+    switch (action) {
+      case 'BootNotification':
+        // Handle bootnotification message
+        console.log('Received BootNotification message:', payload);
+        // Send response back to client
+        await sendResponse(client, connectionId, 'BootNotificationResponse', {
+          status: 'Accepted',
+        });
+        break;
+      case 'StatusNotification':
+        // Handle statusnotification message
+        console.log('Received statusnotification message:', payload);
+        // Send response back to client
+        await sendResponse(client, connectionId, 'StatusNotificationResponse', {
+          status: 'Accepted',
+        });
+        break;
+      default:
+        // Handle unknown message
+        console.log('Received unknown message:', payload);
+        // Send response back to client
+        await sendResponse(client, connectionId, 'ErrorResponse', {
+          error: `Unknown action: ${action}`,
+        });
+    }
+  } catch (error) {
+    console.error('Error handling message:', error);
+    sendResponse(client, connectionId, 'ErrorResponse', { error });
+  }
+};
