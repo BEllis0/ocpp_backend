91d9e3b8d2d30b74f2e849411fa32436e16172b6
jakedave <jakedave@umich.edu>
2023-05-04 22:23:20 -0600

    convert dynamo serverless to sam with stax

---
+++ ops/cf/db/dynamo.rb
@@ -0,0 +1,18 @@
+resource :DynamoDatabase, 'AWS::DynamoDB::Table' do
+  table_name Fn::sub('${app}_${branch}')
+  attribute_definitions [
+    { AttributeName: :connectionId,                   AttributeType: :S }
+  ]
+  key_schema [
+    { AttributeName: :connectionId,                   KeyType: :HASH }
+  ]
+  provisioned_throughput do
+    read_capacity_units 10
+    write_capacity_units 10
+  end
+  # encryption is cool but costs money $$$$
+  # SSE_specification(
+  #   SSEEnabled: true
+  # )
+  tag :Stack, Fn::ref('AWS::StackName')
+end
